6d35a368e3023b838a9bd3284d7643c6fa278ee9
Elias Guderian <46761652+GuderianE@users.noreply.github.com>
2019-05-19 19:29:57 +0200

    added search fuction
    search by captions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
-  $('[data-fancybox="gallery"]').fancybox({
-  }); 
+  $('[data-fancybox="gallery"]').fancybox(); 
 });
-   
+ 
+    let $search = $('#myinput');
+    let $imgs = $('.photo');
+
+$search.keyup(function(event){
+  let val = $search.val().toLowerCase();
+  $imgs.show();
+  if ( val !== '')  {
+    $imgs.not('[data-caption*="' + val + '"]').hide();
+  }
+});
+
